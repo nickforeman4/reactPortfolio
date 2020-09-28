5761b3f2af2994f2ba9b0887305d8fbc26266b13
Nicholas Foreman <nicholas.m.foreman@gmail.com>
2020-09-28 14:10:31 -0500

    Updates

---
+++ precache-manifest.dcf9157bd79f4278e04a62ef83b7978d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55034bd2fc0191f76c2cb789c5abeaa6",
+    "url": "/reactportfolio/index.html"
+  },
+  {
+    "revision": "d14f05de056f6a15064c",
+    "url": "/reactportfolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "933b20f782bb032d78c2",
+    "url": "/reactportfolio/static/js/2.f9fbdff6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactportfolio/static/js/2.f9fbdff6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d14f05de056f6a15064c",
+    "url": "/reactportfolio/static/js/main.6d8ca255.chunk.js"
+  },
+  {
+    "revision": "ccc63777df2cb755b429",
+    "url": "/reactportfolio/static/js/runtime-main.ff3c2836.js"
+  }
+]);
