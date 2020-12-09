0eb828f30be83085fe7d01547d7c46f9ad111206
Nicholas Foreman <nicholas.m.foreman@gmail.com>
2020-12-09 17:53:45 -0600

    Updating index

--- src/components/About.js
+++ src/components/About.js
@@ -18,7 +18,7 @@ function About() {
                                     My name is Nicholas "Nick" Foreman. I am originally from Houston, Texas, but I have done a lot of moving around throughout my lifetime.
                                     I have 2 siblings, and my parents have been together for over 30 years. I attended the Georgia Institute of Technology, where I received
                                     my degree in History, Technology, & Society, and also played basketball. Obviously, I'm a basketball fanatic, but I also enjoy movies,
-                                    travelling, finiding new restaurants, and spending time with family and friends.
+                                    traveling, finiding new restaurants, and spending time with family and friends.
                                 </p>
                             </div>
                         </div>
